02dfca915641fc91965fad9583e75e13d2ab9c21
Yubi48 <yubi48@gmail.com>
2020-10-29 14:41:53 -0400

    chat

---
+++ src/main/java/com/rab3tech/ChatSystemStarter.java
@@ -0,0 +1,12 @@
+package com.rab3tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSystemStarter {
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSystemStarter.class, args);
+	}
+}
---
+++ src/main/java/com/rab3tech/webSocket/controller/WebSocketEventListner.java
@@ -0,0 +1,44 @@
+package com.rab3tech.webSocket.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.rab3tech.webSocket.model.ChatMessage;
+
+@Component
+public class WebSocketEventListner {
+
+	private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListner.class);
+
+	@Autowired
+	private SimpMessageSendingOperations messagingTemplate;
+
+	@EventListener
+	public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+		logger.info("Received a new web socket Connetion");
+	}
+
+	@EventListener
+	public void handleWebSocketDisconnectedListener(SessionDisconnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+		String username = (String) headerAccessor.getSessionAttributes().get("username");
+		if (username != null) {
+			logger.info("User Disconnected: " + username);
+
+			ChatMessage chatMessage = new ChatMessage();
+			chatMessage.setType(ChatMessage.MessageType.LEAVE);
+			chatMessage.setSender(username);
+
+			messagingTemplate.convertAndSend("/topic/public", chatMessage);
+		}
+	}
+
+}
